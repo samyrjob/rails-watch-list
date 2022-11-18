d0d11245893b4d4516713c9b0311b415c68420d5
Samy Rjob <s.rjob@audencia.com>
2022-11-18 13:57:08 +0100

    yeah

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,5 @@
 class BookmarksController < ApplicationController
-  before_action :set_list, only: %i[new create destroy]
+  before_action :set_list, only: %i[new create]
   before_action :set_bookmark, only: :destroy
 
 
